9cba3e0d0ab21c26a88877b64776b2a4e16db283
slavilletta <sergio_pk@hotmail.com>
2024-10-21 18:26:38 -0300

    menu hamb desple

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,8 @@
 import { NavLink } from "react-router-dom";
-import NavBar from "./NavBar.jsx";
+import NavBar from "./NavBar";
 import { Store } from "lucide-react";
-import { useState } from "react";
 
 function Header() {
-  const [clicked, setClicked] = useState(false);
-
-  function handleClick() {
-    setClicked(!clicked);
-  }
-
   return (
     <header className="flex flex-col items-center justify-between min-[774px]:h-[8vh] px-2 font-bold bg-cyan-500 md:flex-row">
       <div className="flex items-center justify-between w-full sm:justify-center md:justify-between">
@@ -19,15 +12,8 @@ function Header() {
             e-Comerce
           </h1>
         </NavLink>
-        <div className="menuBtn" onClick={handleClick}>
-          <div className={clicked ? "" : "hover:transform rotate-45"}></div>
-          <div className={clicked ? "" : "opacity-0"}></div>
-          <div className={clicked ? "" : "hover:transform -rotate-45"}></div>
-        </div>
       </div>
-      <div>
-        <NavBar clicked={clicked} />
-      </div>
+      <NavBar />
     </header>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,74 +3,93 @@ import CartWidget from "./CartWidget";
 import { ChevronDown } from "lucide-react";
 import { useState } from "react";
 
-function NavBar({ clicked }) {
+function NavBar() {
   const [dropdown, setDropdown] = useState(false);
+  const [clicked, setClicked] = useState(false);
+
+  function handleClick() {
+    if (!clicked) {
+      const navbar = document.querySelector(".menuNav");
+      navbar.classList.remove("hidden");
+      navbar.classList.add("flex");
+    } else {
+      const navbar = document.querySelector(".menuNav");
+      navbar.classList.remove("flex");
+      navbar.classList.add("hidden");
+    }
+
+    setClicked(!clicked);
+  }
 
   return (
-    <nav
-      className={`relative flex-col gap-2 text-black menuNav sm:flex sm:flex-row transition-all duration-500 ease-linear ${
-        clicked ? "h-[0vh] opacity-0 overflow-hidden " : "h-[36vh] opacity-100"
-      }`}
-    >
-      <NavLink to="/" className="itemhover">
-        Inicio
-      </NavLink>
-      <NavLink to="/productos" className="itemhover">
-        Productos
-      </NavLink>
-      <div
-        className="relative"
-        onMouseEnter={() => setDropdown(true)}
-        onMouseLeave={() => setDropdown(false)}
-      >
-        <div className="flex items-center cursor-pointer w-[134px] itemhover">
-          Categorías
-          <ChevronDown className="inline-flex ml-1" />
-        </div>
-        {dropdown && (
-          <ul className="z-10 w-32 rounded-b-lg shadow-lg bg-cyan-500 sm:absolute">
-            <li className="block">
-              <NavLink
-                to="/categoria/Remeras"
-                className="block p-2 itemhover hover:bg-cyan-600"
-              >
-                Remeras
-              </NavLink>
-            </li>
-            <li className="block">
-              <NavLink
-                to="/categoria/Camperas"
-                className="block p-2 itemhover hover:bg-cyan-600"
-              >
-                Camperas
-              </NavLink>
-            </li>
-            <li className="block">
-              <NavLink
-                to="/categoria/Pantalones"
-                className="block p-2 itemhover hover:bg-cyan-600"
-              >
-                Pantalones
-              </NavLink>
-            </li>
-            <li className="block">
-              <NavLink
-                to="/categoria/Zapatillas"
-                className="block p-2 itemhover hover:bg-cyan-600"
-              >
-                Zapatillas
-              </NavLink>
-            </li>
-          </ul>
-        )}
+    <>
+      <div className="menuBtn" onClick={handleClick}>
+        <div className={clicked ? "transform rotate-45" : ""}></div>
+        <div className={clicked ? "opacity-0" : ""}></div>
+        <div className={clicked ? "transform -rotate-45" : ""}></div>
       </div>
-      <NavLink to="/checkout" className="block itemhover hover:bg-cyan-600">
-        Contacto
-      </NavLink>
-      <NavLink to="/carrito" className="itemhover">
-        <CartWidget />
-      </NavLink>
-    </nav>
+
+      <nav className="relative flex-col items-center hidden gap-2 text-black menuNav sm:flex sm:flex-row ">
+        <NavLink to="/" className="itemhover">
+          Inicio
+        </NavLink>
+        <NavLink to="/productos" className="itemhover">
+          Productos
+        </NavLink>
+        <div
+          className="relative"
+          onMouseEnter={() => setDropdown(true)}
+          onMouseLeave={() => setDropdown(false)}
+        >
+          <div className="flex items-center cursor-pointer w-[134px] itemhover">
+            Categorías
+            <ChevronDown className="inline-flex ml-1" />
+          </div>
+          {dropdown && (
+            <ul className="z-10 w-32 rounded-b-lg shadow-lg bg-cyan-500 sm:absolute">
+              <li className="block">
+                <NavLink
+                  to="/categoria/Remeras"
+                  className="block p-2 itemhover hover:bg-cyan-600"
+                >
+                  Remeras
+                </NavLink>
+              </li>
+              <li className="block">
+                <NavLink
+                  to="/categoria/Camperas"
+                  className="block p-2 itemhover hover:bg-cyan-600"
+                >
+                  Camperas
+                </NavLink>
+              </li>
+              <li className="block">
+                <NavLink
+                  to="/categoria/Pantalones"
+                  className="block p-2 itemhover hover:bg-cyan-600"
+                >
+                  Pantalones
+                </NavLink>
+              </li>
+              <li className="block">
+                <NavLink
+                  to="/categoria/Zapatillas"
+                  className="block p-2 itemhover hover:bg-cyan-600"
+                >
+                  Zapatillas
+                </NavLink>
+              </li>
+            </ul>
+          )}
+        </div>
+        <NavLink to="/checkout" className="block itemhover hover:bg-cyan-600">
+          Contacto
+        </NavLink>
+        <NavLink to="/carrito" className="itemhover">
+          <CartWidget />
+        </NavLink>
+      </nav>
+    </>
   );
 }
 
